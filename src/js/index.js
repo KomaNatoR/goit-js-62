c693e3a52415b71e86e95110691c2db0e25f6d3d
Serhii Markov <108286639+KomaNatoR@users.noreply.github.com>
2022-09-20 10:50:55 +0300

    create js file

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,13 @@
-console.log('hello');
+
+const x1 = 10;
+const x2 = 30;
+const number = 15;
+
+console.log(`Число ${number} потрапляє в відрізок до ${x1}`, number < x1);
+console.log(`Число ${number} потрапляє в відрізок після ${x2}`, number > x2);
+
+const res1 = number > x1 && number < x2;
+console.log(`Число ${number} потрапляє в відрізок від ${x1} до ${x2}`, res1);
+
+const res2 = number < x1 || number > x2;
+console.log(`Число ${number} потрапляє в відрізок до ${x1} або після ${x2}`, res2);
