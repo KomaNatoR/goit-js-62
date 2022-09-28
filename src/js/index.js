435f84eaa9b7bd203589d52499834ed70b4e3bd6
Serhii Markov <108286639+KomaNatoR@users.noreply.github.com>
2022-09-28 21:29:31 +0300

    qwa

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,6 @@
+function makeArray(firstArray, secondArray, maxLength) {
 
-// const number = 10;
+return firstArray.concat(secondArray).slice(0, maxLength);  
 
-// for (let i = 0; i < number; i += 1) {
-//   if (i % 2 === 0) {
-//     continue;
-//   }
-
-//   console.log("Непарне i: ", i); // 1, 3, 5, 7, 9
-// }
-// =======================================================
-// const min = 3;
-// const max = 33;
-// let total = 0;
-
-// for (let i = min; i <= max; i += 1) {
-//     console.log(i);
-//     if (i % 2 === 0) {
-//         console.log(`even: ${i}`);
-//         total += i;
-//     }
-// }
-// console.log(`sum of even numbers is: ${total}`);
-// =======================================================
+}
+console.log(makeArray([2, 3, 4, 5], [2, 3, 4], 6));
